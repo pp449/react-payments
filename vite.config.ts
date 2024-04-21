e3949a2836129a9534fc3c9070a6549549001fe4
Lee sang Yeop <mma7710@naver.com>
2024-04-21 22:42:28 +0900

    deploy: 배포 파일 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://pp449.github.io/react-payments/dist/",
   plugins: [react()],
 });
